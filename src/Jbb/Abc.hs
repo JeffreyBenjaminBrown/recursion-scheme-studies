439a3c3b34d7b432a52cedd0d1da6847c684988e
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-03-09 15:11:05 -0500

    scrap some boilerplate by using toTerm

--- src/Jbb/Abc.hs
+++ src/Jbb/Abc.hs
@@ -30,18 +30,16 @@ toTerm_abc = ana iso where iso :: Coalgebra AbcF Abc
 
 -- | == `cata` and `ana`
 
-aTerm_abc :: Term AbcF
-aTerm_abc = In $ CF ( In $ BF $ In $ BF ( In $ CF ( In AF)
-                                                  ( In AF)))
-                    ( In $ CF ( In $ BF $ In AF)
-                              ( In $ BF $ In AF))
+an_abc :: Abc
+an_abc =  C ( B $ B (C A A ))
+            ( C (B A) (B A))
 
-forCata :: Algebra AbcF Int -- ^ to demo: `cata forCata aTerm_abc`
+forCata :: Algebra AbcF Int -- ^ to demo: `cata forCata $ toTerm_abc an_abc`
 forCata AF       = 0
 forCata (BF i)   = 1 + i
 forCata (CF i j) = 2 + i + j
 
-forAna :: Coalgebra AbcF Int -- ^ to demo `fromTerm_abc $ ana forAna 0`
+forAna :: Coalgebra AbcF Int -- ^ to demo: `fromTerm_abc $ ana forAna 0`
 forAna i | i < 2     = CF (i+1) (i+1)
          | i < 4     = BF (i+1)
          | otherwise = AF
@@ -49,10 +47,10 @@ forAna i | i < 2     = CF (i+1) (i+1)
 
 -- | == `para`, `para'` and `apo`
 
-aTerm_abc_bbba :: Term AbcF
-aTerm_abc_bbba = In $ BF $ In $ BF $ In $ BF $ In AF
+abc_bbba :: Abc
+abc_bbba = B $ B $ B A
 
-forPara :: RAlgebra AbcF Int -- ^ to demo: `para forPara aTerm_abc_bbba`
+forPara :: RAlgebra AbcF Int -- ^ `para forPara $ toTerm_abc abc_bbba`
 forPara AF                   = 0
 forPara (BF (In (BF _) , i)) = 1 + i*2
 forPara (BF (_         , i)) = 1 + i
@@ -75,11 +73,11 @@ forApo i | i < 4     = BF $ Right $ i+1
 
 -- | `forHisto` gives a 100 point bonus to the longer one,
 -- because at some point in the fold's history it returns 2.
-aTerm_abc_histo, aTerm_abc_histo' :: Term AbcF
-aTerm_abc_histo  = In $ BF $ In $ BF $ In AF
-aTerm_abc_histo' = In $ BF $ In $ BF $ In $ BF $ In AF
+abc_histo, abc_histo' :: Abc
+abc_histo  = B $ B $ A
+abc_histo' = B $ B $ B A
 
-forHisto :: CVAlgebra AbcF Int -- ^ histo forHisto aTerm_abc_histo
+forHisto :: CVAlgebra AbcF Int -- ^ histo forHisto $ toTerm_abc abc_histo`
 forHisto abc = case abc of
   AF -> 0
   BF a -> 1 + attribute a
