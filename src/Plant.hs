9f4506b75029d77aa9ae4b90e79281565575f9cc
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-03-08 21:21:06 -0500

    random plants some changes that were supposed to be committed in the previous commit   according to that previous commit's comment

--- src/Plant.hs
+++ src/Plant.hs
@@ -5,7 +5,7 @@ module Plant where
 
 import System.Random
 
---import Lib
+import Lib
 
 
 data Plant a = Root a     -- every plant starts here
@@ -18,3 +18,27 @@ data Action = Flower  -- stop growing now
             | Branch  -- grow up with a Fork
 data Seed = Seed { height :: Int
                  , rng    :: StdGen }
+
+grow :: Seed -> (Action, Seed, Seed)
+grow (Seed h rand) = ( choose choice
+                     , left { height = h + 1}
+                     , right { height = h + 1} )
+  where (choice :: Int, _)                  = randomR (1 :: Int, 5) rand
+        (leftR :: StdGen, rightR :: StdGen) = split rand
+        (left :: Seed)                      = Seed h leftR
+        (right :: Seed)                     = Seed h rightR
+        choose 1                            = Flower
+        choose 2                            = Branch
+        choose _                            = Upwards
+
+sow :: CVCoalgebra Plant Seed
+sow seed =
+  let (action, left, right) = grow seed
+  in case (action, height seed) of
+    (_, 0)       -> Root (Automatic left)
+    (_, 10)      -> Bloom
+    (Flower, _)  -> Bloom
+    (Upwards, _) -> Stalk (Automatic right)
+    (Branch, _)  -> Fork (Manual (Stalk (Automatic left)))
+                         (Manual Bloom)
+                         (Manual (Stalk (Automatic right)))
